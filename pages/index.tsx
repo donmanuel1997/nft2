534e486d64c8273df947a502f456715304d6014b
donmanuel1997@ <donmanuel1997@gmail.com>
2022-04-02 06:31:51 +0800

    DAY 3 AWESOME

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,117 @@
 import React from 'react'
+import type { GetServerSideProps , NextPage} from 'next'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
 
-function NFTDropPagex() {
+interface Props {
+    collections: Collection[];
+}
+
+const Home = ({ collections }: Props) => {
   return (
-    <div className="flex h-screen flex-col bg-gradient-to-br from-cyan-800 to-slate-600 justify-center ">
-    {/* Left */}
-        <div className='bg-gradient-to-br from-cyan-800 to-slate-600 w-100 h-100'>
-            <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
-               <div className='bg-gradient-to-br from-yellow-50 to-gray-700 rounded-full p-2'>
-                    <img className='w-44 rounded-full object-cover lg:h-86 lg:w-72' src="https://i.imgur.com/nKqUUNw.jpeg" alt="" />
-               </div>
-                <div className='text-center p-5 space-y-2'>
-                    <h1 className='text-4xl text-white font bold'>PRIMEUMATON</h1>
-                    <h2 className='text-xl text-yellow-100'>iCode, iDeploy(), iLuv_my_wife().</h2>
+  <div className="flex h-screen flex-col bg-gradient-to-br from-cyan-800 to-slate-600">
+     <div className="header-2">
+
+<nav className="bg-white py-2 md:py-4">
+  <div className="container px-4 mx-auto md:flex md:items-center">
+
+    <div className="flex justify-between items-center">
+      <a href="#" className="font-bold text-xl text-indigo-600">NFT APES </a>
+      <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
+        <i className="fas fa-bars"></i>
+      </button>
+    </div>
+
+    <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+      <a href="#" className="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600">Recent Sales</a>
+      <a href="#" className="p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-transparent rounded hover:bg-indigo-100 hover:text-indigo-700 transition-colors duration-300">Buy Now</a>
+      <a href="#" className="p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-solid border-indigo-600 rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1"> Contact </a>
+    </div>
+  </div>
+</nav>
+</div>
+    
+<div className="team-2 bg-gray-800 py-6 md:py-12 min-h-screen">
+  <div className="container mx-auto px-4">
+
+    <div className="md:w-10/12 xl:w-8/12 md:mx-auto">
+      <h1 className="font-medium text-3xl md:text-4xl text-white text-center mb-4">NFT APES MARKET PLACE</h1> </div>
+  </div>
+  <div className=" py-6 md:py-1 min-h-screen">
+  <div className="container mx-auto px-4">
+
+    <div className="md:flex md:-mx-2 mt-1 md:mt-1 xl:pt-12 px-1 md:px-0">
+
+      <div className="team-profile-wrap bg-white mx-auto md:w-1/5 px-1 md:py-2">
+        <div className="team-profile text-center p-10">
+          
+          <main>
+        <div>
+            {collections.map(collection => (
+                <a href={`/nft/${collection.slug.current}`}>
+                <div>
+                <div className="w-32 h-34 mx-auto rounded-full overflow-hidden">
+                    <img src={urlFor(collection.mainImage).url()} alt="" />
+                    <div></div>
                 </div>
-                <div className='bg-cyan-900 px-1 py-1'>
-                    <a href="nft/deploy">
-                        <button className=' bg-white px-6 py-2 font-bold text-cyan-900 lg:px-5 lg:py-3 lg:text-base text-xs'> CLICK HERE TO CONTINUE</button>
-                    </a>
+                <p  className="">{collection.title}</p>
+                <p className="text-gray-600 text-sm">{collection.description}</p>
                 </div>
-            </div>
+                </a>
+            ))}
         </div>
-        {/* Right */}
-       
+    </main>
+         
+        </div>
+       </div>
+    </div>
+    </div> 
+</div>
+
+
+      
+
+
+
+  </div>
+   
     </div>
   )
 }
 
-export default NFTDropPagex
+export default Home
 
+export const getServerSideProps: GetServerSideProps = async () => {
+    const query = `*[_type == "collection"]{
+        _id,
+        title,
+        address,
+        description,
+      nftCollectionName,
+        mainImage {
+            asset
+        },
+        circleImage {
+            asset
+        },
+        slug {
+            current
+        },
+        creator-> {
+            _id,
+            name,
+            address,
+            slug {
+                current
+            },
+        },
+    }`
+
+const collections = await sanityClient.fetch(query)
+console.log(collections)
+return {
+    props: {
+        collections
+    }
+}
+}
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
 
-function NFTDropPage() { 
+interface Props {
+    collection: Collection
+}
+function NFTDropPage({collection}: Props) { 
     //   Auth
     const connectWithMetamask = useMetamask()
     const address = useAddress()
@@ -9,16 +16,18 @@ function NFTDropPage() {
     // --
   return (
     // --
+
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+        
     {/* Left */}
         <div className='lg:col-span-3 bg-gradient-to-br from-cyan-800 to-slate-600 '>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
                <div className='bg-gradient-to-br from-yellow-50 to-gray-700 rounded-full p-2'>
-                    <img className='w-44 rounded-full object-cover lg:h-86 lg:w-72' src="https://i.imgur.com/nKqUUNw.jpeg" alt="" />
+                    <img className='w-44 rounded-full object-cover lg:h-86 lg:w-72' src={urlFor(collection.previewImage).url()} alt="" />
                </div>
             <div className='text-center p-5 space-y-2'>
-                <h1 className='text-4xl text-white font bold'>PRIMEUMATON</h1>
-                <h2 className='text-xl text-yellow-100'>iCode, iDeploy(), iLuv_my_wife().</h2>
+                <h1 className='text-4xl text-white font bold'>{collection.title}</h1>
+                <h2 className='text-xl text-yellow-100'>{collection.description}</h2>
             </div>
             </div>
         </div>
@@ -26,11 +35,12 @@ function NFTDropPage() {
         <div className='lg:col-span-7 flex flex-1 flex-col p-12'>
             {/* header */}
             <header className='flex items-center justify-between'>
+                <Link href={'/'}>
                 <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
                     NFT {' '} 
                     <span className='font-extrabold underline decoration-gray-900/50'>METAMASK</span>{' '}LOGIN
                 </h1>
-
+                </Link>
             {address ? (
             <button onClick={() => disconnect()} className=' rounded-full bg-slate-700 px-6 py-2 font-extralight text-white lg:px-5 lg:py-3 lg:text-base text-xs'>Sign Out</button>
             ) : (
@@ -53,7 +63,7 @@ function NFTDropPage() {
                 <img src="https://i.gifer.com/HBZz.gif" alt="" />
                 
                  ) : (
-                 <img src="https://www.esri.com/about/newsroom/wp-content/uploads/2021/06/wherenext-where-in-the-world-Is-crypto-used-wide-1920x1080-1.jpg" alt="" />
+                 <img src={urlFor(collection.mainImage).url()} alt="" />
                  )} 
                 <h1 className=' text-extralight'>Welcome to PRIMEUMATON Community, Connect your METAMASK Wallet to continue.</h1>
                 <p className='pt-2 text-xl'></p>
@@ -72,3 +82,50 @@ function NFTDropPage() {
 
 export default NFTDropPage
 
+export const getServerSideProps: GetServerSideProps = async({params}) => {
+    const query = `*[_type == "collection" && slug.current == $id][0]{
+        _id,
+        title,
+        address,
+        description,
+      nftCollectionName,
+        mainImage {
+            asset
+        },
+        previewImage {
+            asset
+        },
+        circleImage {
+            asset
+        },
+        slug {
+            current
+        },
+        creator-> {
+            _id,
+            name,
+            address,
+            slug {
+                current
+            },
+        },
+    }`
+
+const collection = await sanityClient.fetch(query, {
+    id: params?.id
+})
+
+if (!collection){
+    return{
+        notFound: true
+    }
+}
+
+return {
+    props: {
+        collection
+    }
+}
+
+
+}
